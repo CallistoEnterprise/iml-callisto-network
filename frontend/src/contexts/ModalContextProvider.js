c2c02c2010cdc223abce34f130176bc8b715b517
SunlightLuck <sunlight.luck@gmail.com>
2023-06-20 08:08:46 -0400

    switch first page

--- frontend/src/contexts/ModalContextProvider.js
+++ frontend/src/contexts/ModalContextProvider.js
@@ -4,7 +4,7 @@ export const ModalContext = createContext(null)
 
 export default function ModalContextProvider({ children }) {
   const [openClaimModal, setOpenClaimModal] = useState(true)
-  const [openConnectModal, setOpenConnectModal] = useState(false)
+  const [openConnectModal, setOpenConnectModal] = useState(true)
   const [addresses, setAddresses] = useState([])
   return <ModalContext.Provider value={{ openClaimModal, setOpenClaimModal, openConnectModal, setOpenConnectModal, addresses, setAddresses }}>{children}</ModalContext.Provider>
 };
