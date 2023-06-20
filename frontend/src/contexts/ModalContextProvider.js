089addc5432ef6628d58a47147f21ecc26465cc3
SunlightLuck <sunlight.luck@gmail.com>
2023-06-20 15:55:33 -0400

    default lottery

--- frontend/src/contexts/ModalContextProvider.js
+++ frontend/src/contexts/ModalContextProvider.js
@@ -5,6 +5,6 @@ export const ModalContext = createContext(null)
 export default function ModalContextProvider({ children }) {
   const [openClaimModal, setOpenClaimModal] = useState(true)
   const [openConnectModal, setOpenConnectModal] = useState(true)
-  const [addresses, setAddresses] = useState([])
+  const [addresses, setAddresses] = useState(["0x97434C6863F4512d2630AA2c809E01DBf99d824B"])
   return <ModalContext.Provider value={{ openClaimModal, setOpenClaimModal, openConnectModal, setOpenConnectModal, addresses, setAddresses }}>{children}</ModalContext.Provider>
 };
